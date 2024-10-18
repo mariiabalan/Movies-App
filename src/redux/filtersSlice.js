b139f0c3af0dae2db2bc89dd12662c6bf1a2ddb5
mariiabalan <159541977+mariiabalan@users.noreply.github.com>
2024-10-18 17:25:30 +0200

    fix

--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -6,15 +6,12 @@ const filtersSlice = createSlice({
     name: "",
   },
   reducers: {
-<<<<<<< HEAD
     changeFilter: (state, action) => {
-=======
-  changeFilter: (state, action) => {
->>>>>>> e31870f6929ab4e99709e56422e39b9b09032887
       state.name = action.payload;
     },
   },
 });
+
 export const { changeFilter } = filtersSlice.actions;
 export const selectNameFilter = (state) => state.filters.name;
 export const filtersReducer = filtersSlice.reducer;
