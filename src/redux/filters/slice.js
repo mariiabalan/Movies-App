341ed6c537b0c160972a8630e1e830d599332f6e
mariiabalan <159541977+mariiabalan@users.noreply.github.com>
2024-10-30 22:23:07 +0100

    changes

--- src/redux/filters/slice.js
+++ src/redux/filters/slice.js
@@ -1,21 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const filtersSlice = createSlice({
-//   name: "filters",
-//   initialState: {
-//     name: "",
-//   },
-//   reducers: {
-//     changeFilter: (state, action) => {
-//       state.name = action.payload;
-//     },
-//   },
-// });
-// export const filterReducer = slice.reducer;
-// export const { changeFilter } = filtersSlice.actions;
-// export const selectNameFilter = (state) => state.filters.name;
-// export const filtersReducer = filtersSlice.reducer;
-// import { createSlice } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
